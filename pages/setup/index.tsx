3db9bcd99a14c0eecdadfbbed5ca98c3a26403e8
justxd22 <xdjust18@gmail.com>
2024-09-26 04:47:08 +0300

    fix setup style

--- pages/setup/index.tsx
+++ pages/setup/index.tsx
@@ -8,6 +8,8 @@ import "@/app/assets/styles/login.css";
 import Link from "next/link";
 import { getShipBySessionId } from "@/app/models/Memberships";
 import { House, LayoutDashboard, Bolt, Trash, Pause } from "lucide-react";
+import "@/app/assets/styles/login.css";
+
 
 const TrainingScheduleForm: React.FC = () => {
   const [formData, setFormData] = useState<{ training_days: number[] }>({
@@ -72,23 +74,19 @@ const TrainingScheduleForm: React.FC = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
-      <div className="bg-white shadow-md rounded-lg p-8 max-w-md w-full">
-        <form className="space-y-6" onSubmit={handleSubmit}>
-          <div className="text-center mb-6">
-            <Image src={Logo} alt="Logo" className="mx-auto" />
-            <h1 className="text-2xl font-bold mt-4">Setup</h1>
-          </div>
+    <div className="login-wrapper" style={{ color: "white"}}>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h1 className="login-heading">Setup</h1>
 
           <div className="space-y-4">
-            <label className="block text-gray-700 font-medium">
+            <label className="login-input-box">
               Select your Training Days (3 at least):
             </label>
             <div className="grid grid-cols-4 gap-4">
               {[0, 1, 2, 3, 4, 5, 6].map((day) => (
                 <label
                   key={day}
-                  className="flex flex-col items-center text-gray-700 cursor-pointer"
+                  className="flex flex-col items-center cursor-pointer"
                 >
                   <input
                     type="checkbox"
@@ -108,10 +106,11 @@ const TrainingScheduleForm: React.FC = () => {
               ))}
             </div>
           </div>
-
+          <br></br>
+          <br></br>
           <button
             type="submit"
-            className={`w-full py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 ${
+            className={`login-submit-btn w-full py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 ${
               formData.training_days.length < 3
                 ? "opacity-50 cursor-not-allowed"
                 : ""
@@ -122,7 +121,6 @@ const TrainingScheduleForm: React.FC = () => {
           </button>
         </form>
       </div>
-    </div>
   );
 };
 
